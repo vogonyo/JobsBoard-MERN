f97aa895e93c70f667a8611a58a58e60dba00b8e
vogonyo <vogonyo101@gmail.com>
2021-03-22 12:58:03 +0300

    merged changes

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,13 +2,9 @@ const jwt = require('jsonwebtoken');
 const asyncHandler =require('express-async-handler');
 const User = require('../models/userModel.js');
 
-<<<<<<< HEAD
 
 
 exports.protect = asyncHandler(async (req, res, next) => {
-=======
-const protect = asyncHandler(async (req, res, next) => {
->>>>>>> 5987eb9b465b0b1442c1c4034a6836c9fcbfd3e0
     let token;
   
     if (
@@ -37,25 +33,11 @@ const protect = asyncHandler(async (req, res, next) => {
   });
 
   
-<<<<<<< HEAD
 exports.admin = (req, res, next) => {
-=======
-const admin = (req, res, next) => {
->>>>>>> 5987eb9b465b0b1442c1c4034a6836c9fcbfd3e0
     if (req.user && req.user.isAdmin) {
       next()
     } else {
       res.status(401)
       throw new Error('Not authorized as an admin')
     }
-<<<<<<< HEAD
 };
-=======
-  };
-
-
-module.exports = {
-    protect, admin
-}
-
->>>>>>> 5987eb9b465b0b1442c1c4034a6836c9fcbfd3e0
